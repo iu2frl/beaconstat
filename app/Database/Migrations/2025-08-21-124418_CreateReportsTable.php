ac9739bf56486d57f12a69f7ffe4c832a9484ecc
Luca <61653175+iu2frl@users.noreply.github.com>
2025-08-21 14:46:19 +0200

    Create migration for reports table with necessary fields

---
+++ app/Database/Migrations/2025-08-21-124418_CreateReportsTable.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateReportsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'data' => [
+                'type'       => 'DATETIME',
+                'default'    => 'CURRENT_TIMESTAMP',
+            ],
+            'beacon_id' => [
+                'type'       => 'BIGINT',
+                'constraint' => 20,
+                'unsigned'   => true,
+            ],
+            'callsign' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 10,
+            ],
+            'locator' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 6,
+            ],
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'status' => [
+                'type'       => 'TINYINT',
+                'constraint' => 1,
+            ],
+            'antenna' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 15,
+            ],
+            'note' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+        ]);
+        
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('bs_report', true);
+        
+        // Set table engine to MyISAM and charset to latin1
+        //$this->db->query('ALTER TABLE `bs_report` ENGINE = MyISAM CHARACTER SET latin1 COLLATE latin1_swedish_ci');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('bs_report');
+    }
+}
